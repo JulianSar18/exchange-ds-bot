ea125f87310c1fa314faeb9d7fec81d5a0bbf913
Julian <juliansarmiento10@outlook.com>
2023-01-27 22:20:51 -0500

    refactor

--- src/index.js
+++ src/index.js
@@ -59,69 +59,68 @@ client.on("messageCreate", async (message) => {
     }
   }
 });
-
-
-  async function scrapping () {
-    const browser = await chromium.launch();
-    const page = await browser.newPage();
-    await page.goto(
-      "https://www.superfinanciera.gov.co/inicio/informes-y-cifras/cifras/establecimientos-de-credito/informacion-periodica/diaria/tasa-de-cambio-representativa-del-mercado-trm-60819"
-    );
-    const frame = await page.frameLocator(
-      "#form1 > div.pub > p:nth-child(1) > iframe"
-    );
-    const content = await frame
-      .locator("body > table > tbody > tr.filaPub4 > td:nth-child(3)")
-      .textContent();
-    let testmsg = client.channels.cache.get(process.env.CHANNEL);
-    let date_ob = new Date();
-    let date = ("0" + date_ob.getDate()).slice(-2);
-
-    // current month
-    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-
-    // current year
-    let year = date_ob.getFullYear();
-    const exampleEmbed = new EmbedBuilder()
-      .setColor(0x93c54b)
-      .setTitle("💵 💵 💵 💵 TRM 👀 👀 👀 👀")
-      .setAuthor({
-        name: "TRM-BOT",
-        iconURL:
-          "http://images3.memedroid.com/images/UPLOADED222/62ccc99499c9b.jpeg",
-        url: "https://discord.js.org",
-      })
-      .setDescription(
-        "@everyone Para mi es un placer informarles de las mejores fuentes lo siguiente:"
-      )
-      .setThumbnail(
-        "https://pbs.twimg.com/media/Fe3Zl22WAAESPtl?format=jpg&name=small"
-      )
-      .addFields(
-        {
-          name: "TRM para el dia de HOY",
-          value: `El TRM para **${
-            year + "-" + month + "-" + date
-          }** es de **${content}** COP`,
-        },
-        { name: "\u200B", value: "\u200B" }
-      )
-      .setImage(
-        "https://gustavopetro.co/wp-content/uploads/2022/04/valla2-1.png"
-      )
-      .setTimestamp()
-      .setFooter({
-        text: "Con el auspicio de los cigarrillos que dan risa 🍁🍁🍁🍁🍁",
-        iconURL:
-          "http://images3.memedroid.com/images/UPLOADED222/62ccc99499c9b.jpeg",
-      });
-    testmsg.send({ embeds: [exampleEmbed] });
-    usd = content;
-    await browser.close();
-    return usd;
-  };
 client.login(process.env.DISCORD);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+async function scrapping () {
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+  await page.goto(
+    "https://www.superfinanciera.gov.co/inicio/informes-y-cifras/cifras/establecimientos-de-credito/informacion-periodica/diaria/tasa-de-cambio-representativa-del-mercado-trm-60819"
+  );
+  const frame = await page.frameLocator(
+    "#form1 > div.pub > p:nth-child(1) > iframe"
+  );
+  const content = await frame
+    .locator("body > table > tbody > tr.filaPub4 > td:nth-child(3)")
+    .textContent();
+  let testmsg = client.channels.cache.get(process.env.CHANNEL);
+  let date_ob = new Date();
+  let date = ("0" + date_ob.getDate()).slice(-2);
+
+  // current month
+  let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+
+  // current year
+  let year = date_ob.getFullYear();
+  const exampleEmbed = new EmbedBuilder()
+    .setColor(0x93c54b)
+    .setTitle("💵 💵 💵 💵 TRM 👀 👀 👀 👀")
+    .setAuthor({
+      name: "TRM-BOT",
+      iconURL:
+        "http://images3.memedroid.com/images/UPLOADED222/62ccc99499c9b.jpeg",
+      url: "https://discord.js.org",
+    })
+    .setDescription(
+      "@everyone Para mi es un placer informarles de las mejores fuentes lo siguiente:"
+    )
+    .setThumbnail(
+      "https://pbs.twimg.com/media/Fe3Zl22WAAESPtl?format=jpg&name=small"
+    )
+    .addFields(
+      {
+        name: "TRM para el dia de HOY",
+        value: `El TRM para **${
+          year + "-" + month + "-" + date
+        }** es de **${content}** COP`,
+      },
+      { name: "\u200B", value: "\u200B" }
+    )
+    .setImage(
+      "https://gustavopetro.co/wp-content/uploads/2022/04/valla2-1.png"
+    )
+    .setTimestamp()
+    .setFooter({
+      text: "Con el auspicio de los cigarrillos que dan risa 🍁🍁🍁🍁🍁",
+      iconURL:
+        "http://images3.memedroid.com/images/UPLOADED222/62ccc99499c9b.jpeg",
+    });
+  testmsg.send({ embeds: [exampleEmbed] });
+  usd = content;
+  await browser.close();
+  return usd;
+};
